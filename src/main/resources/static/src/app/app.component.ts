0a929bbce071326473f207064e53e93e7d546972
vrecon <git@vrecon.nl>
2023-02-07 10:17:45 +0100

    added search

--- src/main/resources/static/src/app/app.component.ts
+++ src/main/resources/static/src/app/app.component.ts
@@ -1,40 +1,63 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { SelectItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  fieldNames: any[] = [
-    {label: 'Batch Number', value: 'batchNumber'},
-    {label: 'Product Number', value: 'productNumber'},
-    {label: 'Analyse Type', value: 'analyseType'},
-    {label: 'Username', value: 'username'}
-  ];
-  selectedFieldName: string;
+export class AppComponent implements OnInit {
+  searchForm: FormGroup | undefined;
+  searchLines: FormGroup[] = [];
+  fieldOptions: SelectItem[];
+  operatorOptions: SelectItem[];
+  cols: any[];
 
-  operators: any[] = [
-    {label: 'Greater Than or Equal', value: 'GE'},
-    {label: 'Less Than or Equal', value: 'LE'},
-    {label: 'Equal', value: 'EQUAL'},
-    {label: 'Like', value: 'LIKE'}
-  ];
-  selectedOperator: string;
-
-  value: string;
-
-  searchCriterias: any[] = [];
-
-  addSearchCriteria() {
-    this.searchCriterias.push({
-      fieldName: this.selectedFieldName,
-      operator: this.selectedOperator,
-      value: this.value
-    });
-    this.selectedFieldName = null;
-    this.selectedOperator = null;
-    this.value = null;
+  constructor(private formBuilder: FormBuilder) {
+    this.fieldOptions = [
+      { label: 'Batch Nummer', value: 'batchNummer' },
+      { label: 'Product Nummer', value: 'productNummer' },
+      { label: 'Analyse Type', value: 'analyseType' },
+      { label: 'Username', value: 'username' },
+    ];
+    this.operatorOptions = [
+      { label: 'Greater than or equal', value: 'GE' },
+      { label: 'Less than or equal', value: 'LE' },
+      { label: 'Equal', value: 'EQUAL' },
+      { label: 'Like', value: 'LIKE' },
+    ];
+    this.cols = [
+      { field: 'field', header: 'Field' },
+      { field: 'operator', header: 'Operator' },
+      { field: 'value', header: 'Value' },
+      { field: 'actions', header: '' },
+    ];
   }
+
+  ngOnInit() {
+    this.searchForm =this.formBuilder.group({});
+    this.addSearchLine();
+  }
+
+  addSearchLine() {
+    const newSearchLine = this.formBuilder.group({
+      field: ['', Validators.required],
+      operator: ['', Validators.required],
+      value: ['', Validators.required],
+    });
+    this.searchLines.push(newSearchLine);
+  //  this.searchForm?.addControl(searchLine${this.searchLines.length}, newSearchLine);
+  }
+
+  removeSearchLine(searchLine: FormGroup) {
+    const index = this.searchLines.indexOf(searchLine);
+    this.searchLines.splice(index, 1);
+   // this.searchForm?.removeControl(this.searchLines(index-1));
+  }
+
 }
+
+
+
+
--- src/main/resources/static/src/app/app.module.ts
+++ src/main/resources/static/src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ButtonModule} from "primeng/button";
+import {InputTextModule} from "primeng/inputtext";
+import {DropdownModule} from "primeng/dropdown";
+import {ReactiveFormsModule} from "@angular/forms";
+import {TableModule} from "primeng/table";
+import {CardModule} from "primeng/card";
 
 @NgModule({
   declarations: [
@@ -10,7 +16,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    InputTextModule,
+    DropdownModule,
+    ReactiveFormsModule,
+    TableModule,
+      CardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
